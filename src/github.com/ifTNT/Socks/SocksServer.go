6ccabb22f871710d0b56b9be5ac29542c074c28c
ifTNT <happyjohn369@gmail.com>
2014-08-22 15:26:58 +0800

    move SocksConn to another file

--- src/github.com/ifTNT/Socks/SocksServer.go
+++ src/github.com/ifTNT/Socks/SocksServer.go
@@ -110,34 +110,3 @@ func (s *SocksServer) preHandle(conn *net.TCPConn, ch_data chan []byte, ch_err c
 func (s *SocksServer) Stop() {
 	s.server.Close()
 }
-
-type SocksConn struct {
-	RemoteAddr struct {
-		Host string
-		Port int
-	}
-	conn     *net.TCPConn
-	writeBuf *bytes.Buffer
-	mother   *SocksServer
-}
-
-func (s *SocksConn) RemoteAddr2String() string {
-	return s.RemoteAddr.Host + ":" + strconv.Itoa(s.RemoteAddr.Port)
-}
-func (s *SocksConn) SetCanConnect(rep byte) {
-	s.conn.Write([]byte{VER, rep, RSV, IPv4, 0, 0, 0, 0, 0, 0})
-}
-
-func (s *SocksConn) Write(willWrite []byte) {
-	s.writeBuf.Write(willWrite)
-}
-func (s *SocksConn) Flush() {
-	if _, err := s.conn.Write(s.writeBuf.Bytes()); err != nil {
-		fmt.Println("Write to " + s.RemoteAddr2String() + " Failed")
-		err.Error()
-	}
-	s.writeBuf.Reset()
-}
-func (s *SocksConn) Close() {
-	s.conn.Close()
-}
